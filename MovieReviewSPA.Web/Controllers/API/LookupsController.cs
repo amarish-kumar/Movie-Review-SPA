e9fc553f1dd3eb05ed9b2d318555a23fe931ee1f
rahulsahay19 <rahulsahay19@gmail.com>
2016-03-30 23:18:35 +0530

    Chapter 3 completed

--- MovieReviewSPA.Web/Controllers/API/LookupsController.cs
+++ MovieReviewSPA.Web/Controllers/API/LookupsController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using MovieReviewSPA.Data.Contracts;
+using MovieReviewSPA.Model;
+using System.Linq;
 
 namespace MovieReviewSPA.Web.Controllers.API
 {
+    [Route("api/[controller]")]
     public class LookupsController : Controller
     {
-        public LookupsController()
+        private readonly IMovieReviewUow UOW;
+
+        public LookupsController(IMovieReviewUow uow)
         {
-            
+            UOW = uow;
         }
+
+        // GET: api/lookups/movies
+        [HttpGet("movies")]
+        public IEnumerable<Movie> GetMovies()
+        {
+            return UOW.Movies.GetAll().OrderBy(m => m.Id);
+        }
+
+        // /api/Lookups/getbyreviewerid?id=1
+        [HttpGet("getbyreviewerid")]
+        public MovieReview GetByReviewerId(int id)
+        {
+            return UOW.MovieReviews.GetById(id);
+        }
+
+        #region OData Future: IQueryable<T>
+        //[Queryable]
+        // public IQueryable<Movie> Get()        
+        // public IQueryable<MovieReview> Get()
+
+        #endregion
+
+
     }
 }
