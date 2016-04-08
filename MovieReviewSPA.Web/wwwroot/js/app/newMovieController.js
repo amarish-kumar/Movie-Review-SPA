93439c18c839b04b7382e849069501fc823a4adf
rahulsahay19 <rahulsahay19@gmail.com>
2016-04-08 11:05:12 +0530

    Chapter - 5 - 3rd Stage

--- MovieReviewSPA.Web/wwwroot/js/app/newMovieController.js
+++ MovieReviewSPA.Web/wwwroot/js/app/newMovieController.js
@@ -2,5 +2,13 @@
 (function() {
     "use strict";
 
+    angular.module("moviesApp").controller("newMovieController", newMovieController);
 
-}())
+    function newMovieController($scope) {
+        $scope.newMovie = {};
+        $scope.save = function () {
+            toastr.success("Movie Saved Successfully");
+        }
+
+    }
+}());
